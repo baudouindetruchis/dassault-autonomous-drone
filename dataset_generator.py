33159a48f587325fa9407cffca11b3bbf3288639
Baudouin de Truchis <baudouin@detruchis.com>
2020-01-27 01:01:52 +0100

    update

--- dataset_generator.py
+++ dataset_generator.py
@@ -1,8 +1,10 @@
-from PIL import Image
+import numpy as np
 from random import randint
+from PIL import Image, ImageEnhance, ImageFilter
+import cv2
 
 # ========== INFORMATION ==========
-# --> models smaller than 300x300px
+# input model size = 200x200px
 # =================================
 
 def random_image(model, background):
@@ -13,6 +15,20 @@ def random_image(model, background):
     model_width, model_height = model.size
     scale = randint(10,100)/100
     model.thumbnail((model_width*scale, model_height*scale), Image.ANTIALIAS)
+
+    # Random brithness
+    enhancer = ImageEnhance.Brightness(model)
+    model = enhancer.enhance(randint(50,100)/100)
+
+    # Random gaussian_noise
+    alpha = model.split()[-1]
+    model = model.convert('RGB')
+    model_array = cv2.cvtColor(np.array(model), cv2.COLOR_RGB2BGR)              # Convert to array and swap RGB --> BGR
+    print(model_array.max())
+    noise = np.random.normal(loc=0, scale=1, size=model_array.shape)
+    model_array = np.clip((model_array + noise).astype('uint8'),0,255)
+    model = Image.fromarray(cv2.cvtColor(model_array, cv2.COLOR_BGR2RGB))       # Swap BGR --> RGB and convert to pillow
+    model.putalpha(alpha)
 
     # Random paste : model --> background
     model_width, model_height = model.size
@@ -33,4 +49,10 @@ generated = random_image(image_model, generated)
 generated = random_image(image_model, generated)
 generated = random_image(image_model, generated)
 generated = random_image(image_model, generated)
+generated = random_image(image_model, generated)
+generated = random_image(image_model, generated)
+
 generated.show()
+
+# model_array = cv2.cvtColor(np.array(image_model), cv2.COLOR_RGB2BGR)
+# print(model_array.min())
